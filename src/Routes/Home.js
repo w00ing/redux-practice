9ba6575ea87831d201b979d054cd22b60767bc50
Woo-yeong Kim <kimwy1997@gmail.com>
2020-09-10 02:02:10 +0900

    Finish ToDos with react-redux

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import { add } from "../store";
+import ToDo from "../Components/ToDo";
 
-function Home() {
+function Home({ toDos, addToDo }) {
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
   }
   function onSubmit(e) {
     e.preventDefault();
+    addToDo(text);
     setText("");
   }
   return (
@@ -16,9 +20,23 @@ function Home() {
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
-      <ul></ul>
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo {...toDo} key={toDo.id} />
+        ))}
+      </ul>
     </>
   );
 }
 
-export default Home;
+function mapStateToProps(state) {
+  return { toDos: state };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: (text) => dispatch(add(text)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
